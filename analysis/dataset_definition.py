65ce78f737caa933be27a2a96812e07b1c397c10
Milan Wiedemann <milan.wiedemann@gmail.com>
2026-02-11 13:53:18 +0000

    Update demo and results

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -17,12 +17,12 @@ dataset.aged_17_or_older = dataset.age >= 17
 depression_codes = codelist_from_csv(
     "codelists/nhsd-primary-care-domain-refsets-depr_cod.csv",
     column="code",
-    )
+)
 
 depression_resolved_codes = codelist_from_csv(
     "codelists/nhsd-primary-care-domain-refsets-depres_cod.csv",
     column="code",
-    )
+)
 
 dataset.last_dep_diagnosis_date = (
     clinical_events.where(clinical_events.snomedct_code.is_in(depression_codes))
@@ -42,15 +42,6 @@ dataset.last_dep_resolved_date = (
 
 dataset.has_unresolved_depression = dataset.last_dep_diagnosis_date.is_not_null() & (
     dataset.last_dep_resolved_date.is_null() | (dataset.last_dep_resolved_date < dataset.last_dep_diagnosis_date)
-)
-
-# PHQ9 score
-dataset.latests_phq9_score = (
-    clinical_events.except_where(clinical_events.snomedct_code.is_in(depression_codes))
-    .sort_by(clinical_events.date)
-    .where(clinical_events.date <= index_date)
-    .last_for_patient()
-    .numeric_value
 )
 
 dataset.define_population(dataset.is_alive & dataset.aged_17_or_older & dataset.has_unresolved_depression)
--- analysis/dataset_definition_demo.py
+++ analysis/dataset_definition_demo.py
@@ -1,5 +1,5 @@
 from ehrql import show, create_dataset, codelist_from_csv
-from ehrql.tables.core import patients, clinical_events, practice_registrations
+from ehrql.tables.core import patients, clinical_events
 
 index_date = "2024-03-31"
 
@@ -8,8 +8,11 @@ dataset.configure_dummy_data(population_size=1000)
 
 # Patient is alive
 
-# Patient is aged 17 years or older 
+# Patient is aged 17 years or older
 
 # Patient has unresolved diagnosis of depression
 
-show(patients)
+# Patient has unresolved diagnosis of depression
+
+
+show(dataset)
